a064befab22628494aaeb2cf4f3b912452d4920e
Irfan-XD <114362956+irfan-XD@users.noreply.github.com>
2022-11-24 00:06:36 +0700

    Add files via upload

---
+++ FBP.py
@@ -0,0 +1,37 @@
+#coding by irfan azhura XD
+#irfan ganteng anjay selebwwww
+import requests, time, random, os, re, sys, json
+from rich import print as prints
+from rich.console import Console
+from rich.panel import Panel
+from rich.panel import Panel as nel
+from rich import print as cetak
+x = '\33[m' # DEFAULT
+m = '\x1b[1;91m' #RED +
+k = '\033[93m' # KUNING +
+h = '\x1b[1;92m' # HIJAU +
+hh = '\033[32m' # HIJAU -
+u = '\033[95m' # UNGU
+kk = '\033[33m' # KUNING -
+N = '\x1b[0m'    # WARNA MATI
+A = '\x1b[1;90m' # WARNA ABU ABU
+o = 'FF1493'
+def clear():
+	os.system(f'clear')
+	back()
+def AZHURA():
+    os.system('clear')
+    Console(width=35, style="cyan").print(Panel("""[bold white]
+╔═╗╔╗ ╔═╗╦═╗╔═╗╔╦╗
+╠╣ ╠╩╗╠═╝╠╦╝║╣ ║║║
+╚  ╚═╝╩  ╩╚═╚═╝╩ ╩
+[bold green][By irfan azhura]""", title="[bold green] • V.1.0 •"), justify="center")
+print('')
+AZHURA()
+Console(width=35,style="bold cyan").print(Panel("[bold default]perbulan  : 120k\nperminggu : 60k\npermanen+code source : 300k ", title="•"))
+Console(width=35,style="bold cyan").print(Panel("[bold white][[bold green]1[bold default]].Beli apikey \n[[bold green]0[bold default]].logout  ", title="•"))
+irfanXD = input(f' [{h}•{x}] pilih : ')
+if irfanXD in ['1']:
+     os.system('xdg-open https://Wa.me/+085830480041')
+elif irfanXD in ['0']:
+     os.system('xdg-open https://bokepind.net/?s=Jepang+')
